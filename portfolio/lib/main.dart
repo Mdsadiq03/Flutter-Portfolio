abd908f48b24a8f3af709ffd71f41ab93c452dad
Mdsadiq03 <mohamedsadiq2k3@gmail.com>
2024-09-28 23:06:23 +0530

    optimization

--- portfolio/lib/main.dart
+++ portfolio/lib/main.dart
@@ -4,10 +4,12 @@ import 'package:google_fonts/google_fonts.dart';
 import 'constants.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
