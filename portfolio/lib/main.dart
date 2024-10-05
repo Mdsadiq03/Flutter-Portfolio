4be1d52310aed7471437c7b8b6598beaf2ee68aa
Mdsadiq03 <mohamedsadiq2k3@gmail.com>
2024-10-05 13:29:16 +0530

    title and icon updation

--- portfolio/lib/main.dart
+++ portfolio/lib/main.dart
@@ -1,21 +1,26 @@
+import 'package:firebase_core/firebase_core.dart';
+
 import 'package:flutter/material.dart';
 import 'package:portfolio/screens/home/home_screen.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'constants.dart';
 
 void main() {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
+
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Portfolio',
+      title: 'Sadiq | Flutter Portfolio',
       theme: ThemeData.dark().copyWith(
         primaryColor: primaryColor,
         scaffoldBackgroundColor: bgColor,
@@ -27,7 +32,18 @@ class MyApp extends StatelessWidget {
               bodyMedium: const TextStyle(color: bodyTextColor),
             ),
       ),
-      home:  HomeScreen(),
+      home: FutureBuilder(
+          future: _initialization,
+          builder: (context, snapshot) {
+            if(snapshot.hasError) {
+              print("Error");
+            }
+            if (snapshot.connectionState == ConnectionState.done) {
+              return HomeScreen();
+            }
+            return const CircularProgressIndicator();
+          },
+      ),
     );
   }
 }
